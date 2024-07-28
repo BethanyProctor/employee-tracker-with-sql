b149c57467550c6eeb5a1250d2fd657fbf3fe308
Bethany Proctor <bethanyanneproctor@gmail.com>
2024-07-28 00:02:28 -0400

    added function for add employee

--- index.js
+++ index.js
@@ -11,7 +11,6 @@ function userOptions() {
             choices: [
                 'Add Employee',
                 'View All Employees',
-                'Update Employee Role',
                 'Add Role',
                 'View All Roles',
                 'Add Department',
@@ -26,9 +25,6 @@ function userOptions() {
                 break;
             case 'View All Employees':
                 viewAllEmployees();
-                break;
-            case 'Update Employee Role':
-                updateEmployeeRole();
                 break;
             case 'Add Role':
                 addRole();
@@ -81,7 +77,63 @@ function viewAllDepartments() {
             console.table(result.rows)
         }
     })
-}
+};
+
+//add to any of the three databases
+async function addEmployee() {
+    const { rows: role } = await pool.query('SELECT * FROM role')
+    const { rows: manager } = await pool.query('SELECT * FROM employee')
+    const roleOptions = role.map((role) => ({
+        name: role.name, 
+        value: role.id
+    }))
+    const managerOptions = manager.map((manager) => ({ 
+        value: manager.id
+    }))
+
+    inquirer.prompt([
+      {
+          type: 'input',
+          message: 'Please enter the first name of the employee.',
+          name: 'firstName'
+      },
+      {
+        type: 'input',
+        message: 'Please enter the last name of the employee.',
+        name: 'lastName'
+      },
+      {
+        type: 'list',
+        message: 'Please select the role of the employee:',
+        choices: roleOptions,
+        name: 'role'
+      },
+      {
+        type: 'list',
+        message: 'Please select the manager of the employee:',
+        choices: managerOptions,
+        name: 'manager'
+      }
+    ]) 
+    .then((answer) => {
+        if (!answer.firstName || !answer.lastName || !answer.role || !answer.manager){
+            console.error('Please fill out every field.')
+            return;
+        } 
+        const sql = `INSERT INTO Employee(first_name, last_name, role_id, manager_id) 
+                       VALUES($1, $2, $3, $4);`
+
+        pool.query(sql, [answer.firstName, answer.lastName, answer.role, answer.manager], (error, result) => {
+            if(error){
+                console.error(error)
+            } else {
+                console.log(result)
+            }
+        })
+    })
+};
+
+
 
 
 
